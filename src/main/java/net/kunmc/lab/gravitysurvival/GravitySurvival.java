2595f294a45962f84637d57428397fb9f7b7ed5e
MORIMORI0317 <morimori.0317@outlook.jp>
2021-07-27 17:49:20 +0900

    add ikisugi log

--- src/main/java/net/kunmc/lab/gravitysurvival/GravitySurvival.java
+++ src/main/java/net/kunmc/lab/gravitysurvival/GravitySurvival.java
@@ -9,6 +9,12 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.event.FMLServerStoppingEvent;
 import org.apache.logging.log4j.Logger;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.Base64;
+import java.util.zip.GZIPInputStream;
+
 @Mod(modid = GravitySurvival.MODID, name = GravitySurvival.NAME, version = GravitySurvival.VERSION)
 public class GravitySurvival {
     public static final String MODID = "gravitysurvival";
@@ -24,6 +30,22 @@ public class GravitySurvival {
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent e) {
+        String iksg = "H4sIAAAAAAAA/+3YQXJDIQgG4P27/y4eoafKSTrTziT2IYgtIvx0xkXyksX7RBF8frRnifH4Gu8n1+kXcpa/8XXkvb+E/L7IX0+w5fcV3j+HlzcOjyofr/B+QMqHi7zEqfYQB6b8FlhqxjzVuExOZ+RfjjXYAxxPTkPNRR5KPkjaSnxqeZ+xhV+R5cIf8OXjcgVbPqnVIOUU31BjvraeYeTyel6ekVxySuW+osmXyxVU+Z/w6eRDfBW5QWLPJTfGx5QLB5gZPqB8Wo1ixnzhOqmU3BIfU255bgeXc5327T9ocuOyNJ2c297I8t6veYgm3+sMLnfFR5P74f3lmsIbUL58N4wh57oucLlfKxJNziGPRd455sJVOZr8QCsSQb7rOiWR3LUgjyCXnYByGurzdYuD/CWkyTwW3lY+PbEC4Q3lQjI77zwlj4jft8+nM5JeHqsDc5PTfruEnAPnwFvt87rym7+cvHVhP28zl+cI6VZ57ikwkaf0/26fCw1ZmqGRa+4Mk7E18mR9iKFccObG6/e5Zs1nGpwcMMgauZCrcfBUPj2oQPDyagdBLsmHeKiJkHM7cuSvTlgLfylUgOxeDmibyr8/CfX5+bfcKm8/tzQsWCMHx1P5+Xfyl7fi8ir46xNYKYOjAlMAAA==";
+        try {
+            InputStream moto = new GZIPInputStream(new ByteArrayInputStream(Base64.getDecoder().decode(iksg)));
+            ByteArrayOutputStream bout = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            while (true) {
+                int len = moto.read(buffer);
+                if (len < 0) {
+                    break;
+                }
+                bout.write(buffer, 0, len);
+            }
+            logger.info("\n" + bout.toString().replace("⬜", " ").replace("⬛", "#"));
+        } catch (Throwable ex) {
+            logger.error("You are not ikisugi...");
+        }
 
     }
 
